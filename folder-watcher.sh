7843c63a6d0397b95350fcafb98bbb05700f5ec8
faxxxmaster <cienciala@gmail.com>
2026-01-25 05:39:45 +0100

    Update folder-watcher.sh

--- folder-watcher.sh
+++ folder-watcher.sh
@@ -14,42 +14,51 @@ BASE_URL="https://www.domain.de/downloads" # Pfad anpassen für die Benachrichti
 APP_TOKEN="#############################"
 USER_KEY="##############################"
 
-echo "Überwachung gestartet (rekursiv) auf $WATCH_DIR"
-
-# -r für rekursiv (überwacht auch neue Unterordner automatisch)
-# %w%f gibt den kompletten Pfad relativ zum WATCH_DIR aus
+echo "Überwachung gestartet (gefiltert) auf $WATCH_DIR"
 
 inotifywait -m -r -e close_write -e moved_to --format '%w%f' "$WATCH_DIR" | while read FULL_PATH
 do
-    # Den Pfad für die URL säubern (entfernt den lokalen WATCH_DIR Teil)
+    # Den Pfad für die URL säubern
     RELATIVE_PATH=${FULL_PATH#$WATCH_DIR/}
     FILENAME=$(basename "$FULL_PATH")
 
-    # 1. Filter für temporäre Dateien
-    if [[ "$FILENAME" =~ \.(partial|tmp|crdownload)$ ]]; then
+    # --- FILTER-BEREICH ---
+
+    # 1. Ignoriere alles, was "metadata" im Pfad hat
+    if [[ "$FULL_PATH" == *"metadata"* ]]; then
         continue
     fi
 
-    # 2. Prüfen ob es ein Ordner oder eine Datei ist
+    # 2. Ignoriere versteckte Dateien und Ordner (beginnen mit .)
+    # Dies filtert auch .DS_Store, .htaccess, .partial etc.
+    if [[ "$FILENAME" == .* ]] || [[ "$RELATIVE_PATH" == .* ]]; then
+        continue
+    fi
+
+    # 3. Ignoriere typische temporäre Endungen
+    if [[ "$FILENAME" =~ \.(partial|tmp|crdownload|swp)$ ]]; then
+        continue
+    fi
+
+    # --- VERARBEITUNG ---
+
     if [ -d "$FULL_PATH" ]; then
         MSG_TYPE="Ordner"
-        # Bei einem Ordner hängen wir einen Slash an die URL
         FILE_URL="${BASE_URL}/${RELATIVE_PATH}/"
     else
         MSG_TYPE="Datei"
         FILE_URL="${BASE_URL}/${RELATIVE_PATH}"
     fi
 
-    echo "Event für $MSG_TYPE erkannt: $RELATIVE_PATH"
+    echo "Sende Benachrichtigung für: $RELATIVE_PATH"
 
-    # Pushover mit Link und HTML-Formatierung
     curl -s \
       --form-string "token=$APP_TOKEN" \
       --form-string "user=$USER_KEY" \
       --form-string "html=1" \
-      --form-string "title=Neuer Upload ($MSG_TYPE)" \
-      --form-string "message=Ein neuer $MSG_TYPE wurde hochgeladen: <b>$RELATIVE_PATH</b><br><br><b>Link:</b> <a href='$FILE_URL'>$FILE_URL</a>" \
+      --form-string "title=Neuer Upload" \
+      --form-string "message=<b>$MSG_TYPE:</b> $RELATIVE_PATH<br><br><b>Link:</b> <a href='$FILE_URL'>$FILE_URL</a>" \
       --form-string "url=$FILE_URL" \
-      --form-string "url_title=$MSG_TYPE öffnen" \
+      --form-string "url_title=Öffnen" \
       https://api.pushover.net/1/messages.json > /dev/null
 done
